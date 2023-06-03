92fe525008ab64135cf293883c4d6211e14c6dc8
serialt <tserialt@gmail.com>
2023-06-03 14:27:30 +0800

    feat: add sqlite

--- db.go
+++ db.go
@@ -1,100 +1,26 @@
 package db
 
 import (
-	"errors"
-	"fmt"
-	"time"
-
 	"github.com/glebarez/sqlite"
-	"go.uber.org/zap"
-	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
+
+	"golang.org/x/exp/slog"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
-	"moul.io/zapgorm2"
 )
 
-type Database struct {
-	Type            string
-	Addr            string
-	Port            string
-	DBName          string
-	Username        string
-	Password        string
-	MaxIdleConns    int           // 设置空闲连接池中连接的最大数量
-	MaxOpenConns    int           // 设置打开数据库连接的最大数量
-	ConnMaxLifetime time.Duration // 设置连接可复用的最大时间。
-}
+func New(file string) *gorm.DB {
 
-// 设置gorm日志使用zap
+	gromdb, err := gorm.Open(sqlite.Open(file), &gorm.Config{
 
-// NewDBConnect 获取gorm.DB
-func (db *Database) NewDBConnect(zaplog *zap.Logger) (GormDB *gorm.DB, err error) {
-	// 使用zap 接收gorm日志
-	GormLogger := zapgorm2.New(zaplog)
-	GormLogger.SetAsDefault()
-
-	var dialector gorm.Dialector
-	switch db.Type {
-	case "sqlite":
-		dialector = sqlite.Open(db.DBName)
-
-	case "mysql":
-		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-			db.Username,
-			db.Password,
-			db.Addr,
-			db.Port,
-			db.DBName,
-		)
-		dialector = mysql.New(mysql.Config{
-			DSN:                       dsn,
-			DefaultStringSize:         256,   // string 类型字段的默认长度
-			DisableDatetimePrecision:  true,  // 禁用 datetime 精度，MySQL 5.6 之前的数据库不支持
-			DontSupportRenameIndex:    true,  // 重命名索引时采用删除并新建的方式，MySQL 5.7 之前的数据库和 MariaDB 不支持重命名索引
-			DontSupportRenameColumn:   true,  // 用 `change` 重命名列，MySQL 8 之前的数据库和 MariaDB 不支持重命名列
-			SkipInitializeWithVersion: false, // 根据当前 MySQL 版本自动配置
-		})
-	case "postgresql":
-		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-			db.Addr,
-			db.Username,
-			db.Password,
-			db.DBName,
-			db.Port,
-		)
-		dialector = postgres.New(postgres.Config{
-			DSN:                  dsn,
-			PreferSimpleProtocol: true, // disables implicit prepared statement usage,
-		})
-	default:
-		return nil, errors.New("The database is not supported, please choice [sqlite],[mysql] or [postgresql]")
-	}
-	GormDB, err = gorm.Open(dialector, &gorm.Config{
-		Logger:                                   GormLogger,
 		DisableForeignKeyConstraintWhenMigrating: true,
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true, // 设置创建表名时不使用复数
 		},
 	})
-	if db.MaxOpenConns != 0 {
-		// Gorm 使用database/sql 维护连接池
-		sqlDB, _ := GormDB.DB()
-		// 设置空闲连接池中连接的最大数量
-		sqlDB.SetMaxIdleConns(db.MaxIdleConns)
-		// 设置打开数据库连接的最大数量
-		sqlDB.SetMaxOpenConns(db.MaxOpenConns)
-		// 设置了连接可复用的最大时间
-		sqlDB.SetConnMaxLifetime(db.ConnMaxLifetime)
-	} else {
-		sqlDB, _ := GormDB.DB()
-		// SetMaxIdleConns 设置空闲连接池中连接的最大数量
-		sqlDB.SetMaxIdleConns(10)
-		// SetMaxOpenConns 设置打开数据库连接的最大数量。
-		sqlDB.SetMaxOpenConns(100)
-		// SetConnMaxLifetime 设置连接可复用的最大时间。
-		sqlDB.SetConnMaxLifetime(time.Hour)
+	if err != nil {
+		slog.Info("open db failed", "error", err)
 	}
 
-	return
+	return gromdb
+
 }
--- db_test.go
+++ db_test.go
@@ -1,26 +1,26 @@
 package db
 
-import (
-	"testing"
+// import (
+// 	"testing"
 
-	"github.com/serialt/sugar"
-)
+// 	"github.com/serialt/sugar"
+// )
 
-func TestGetGormDB(t *testing.T) {
-	mydb := &Database{
-		Type:     "mysql",
-		Addr:     "10.0.16.10",
-		Port:     "3306",
-		DBName:   "mysql",
-		Username: "root",
-		Password: "centos",
-	}
-	_, err := mydb.NewDBConnect(sugar.NewLogger("debug", "", "", false))
-	// DB.AutoMigrate(&Department{})
-	// DB.AutoMigrate(&Userlist{})
-	if err != nil {
-		t.Errorf("DB connect failed: %v", err)
-	} else {
-		t.Logf("DB connect succeed: %v", err)
-	}
-}
+// func TestGetGormDB(t *testing.T) {
+// 	mydb := &Database{
+// 		Type:     "mysql",
+// 		Addr:     "10.0.16.10",
+// 		Port:     "3306",
+// 		DBName:   "mysql",
+// 		Username: "root",
+// 		Password: "centos",
+// 	}
+// 	_, err := mydb.NewDBConnect(sugar.NewLogger("debug", "", "", false))
+// 	// DB.AutoMigrate(&Department{})
+// 	// DB.AutoMigrate(&Userlist{})
+// 	if err != nil {
+// 		t.Errorf("DB connect failed: %v", err)
+// 	} else {
+// 		t.Logf("DB connect succeed: %v", err)
+// 	}
+// }
