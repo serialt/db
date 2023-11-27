4e515aceaa5204cfeda4095a8d42d626542b5026
serialt <tserialt@gmail.com>
2023-11-28 01:01:23 +0800

    fix

--- db.go
+++ db.go
@@ -3,25 +3,19 @@ package db
 import (
 	"github.com/glebarez/sqlite"
 
-	"log/slog"
-
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
 
-func New(file string) *gorm.DB {
-
-	gromdb, err := gorm.Open(sqlite.Open(file), &gorm.Config{
-
+func New(file string) (gromdb *gorm.DB, err error) {
+	GormLoger := SetSlog()
+	gromdb, err = gorm.Open(sqlite.Open(file), &gorm.Config{
+		Logger:                                   GormLoger,
 		DisableForeignKeyConstraintWhenMigrating: true,
 		NamingStrategy: schema.NamingStrategy{
 			SingularTable: true, // 设置创建表名时不使用复数
 		},
 	})
-	if err != nil {
-		slog.Info("open db failed", "error", err)
-	}
-
-	return gromdb
+	return
 
 }
--- db_test.go
+++ db_test.go
@@ -1,26 +1,10 @@
 package db
 
-// import (
-// 	"testing"
+import "testing"
 
-// 	"github.com/serialt/sugar"
-// )
-
-// func TestGetGormDB(t *testing.T) {
-// 	mydb := &Database{
-// 		Type:     "mysql",
-// 		Addr:     "10.0.16.10",
-// 		Port:     "3306",
-// 		DBName:   "mysql",
-// 		Username: "root",
-// 		Password: "centos",
-// 	}
-// 	_, err := mydb.NewDBConnect(sugar.NewLogger("debug", "", "", false))
-// 	// DB.AutoMigrate(&Department{})
-// 	// DB.AutoMigrate(&Userlist{})
-// 	if err != nil {
-// 		t.Errorf("DB connect failed: %v", err)
-// 	} else {
-// 		t.Logf("DB connect succeed: %v", err)
-// 	}
-// }
+func TestGetGormDB(t *testing.T) {
+	_, err := New("test.db")
+	if err != nil {
+		t.Fatal("open db failed")
+	}
+}
