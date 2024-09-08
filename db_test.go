335953e8057edc58677775a2302d3511fd4f2473
serialt <tserialt@gmail.com>
2024-09-08 11:59:30 +0800

    fix: sqlite

--- db_test.go
+++ db_test.go
@@ -1,10 +1,29 @@
 package db
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGetGormDB(t *testing.T) {
-	_, err := New("test.db")
+
+	type User struct {
+		Name string
+		Age  int
+	}
+
+	sqlDB, err := New("test.db")
 	if err != nil {
 		t.Fatal("open db failed")
+		panic(err)
 	}
+	sqlDB.AutoMigrate(&User{})
+
+	jerry := &User{Name: "jerry", Age: 18}
+
+	sqlDB.Model(&User{}).Create(&jerry)
+
+	newJerry := &User{}
+
+	sqlDB.Model(&User{}).Where("name = ?", "jerry").First(&newJerry)
+	t.Logf("Get user from db, data=%v", newJerry)
 }
